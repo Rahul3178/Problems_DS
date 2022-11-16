c89590a6afdb3a1ee8d731f1efb2a743e38bf8f7
Rahul <rk2670382@gmail.com>
2022-11-16 12:15:16 +0530

    we dont need any class whic implements functional; interface we use referensce of functional interface to use lambda expression.

--- src/LeetCode/finterface.java
+++ src/LeetCode/finterface.java
@@ -3,21 +3,22 @@ package LeetCode;
 public class finterface
 {
     public static void main(String[] args) {
-        myface f= new demo();
+        myface f=()-> System.out.println("Hello lambda expression");
         f.show();
     }
 
 
 }
-
+@FunctionalInterface
 interface  myface {
     abstract void show();
 }
 
-class demo implements myface{
+// we only need  finctional interface to use lambda expression;
+/*class demo implements myface{
 
     @Override
     public void show() {
         System.out.println("Hello Rahul");
     }
-}
+}*/
