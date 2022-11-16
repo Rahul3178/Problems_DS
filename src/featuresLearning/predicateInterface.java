d70fe89d737434bc94130676d802580bdfb8a675
Rahul <rk2670382@gmail.com>
2022-11-16 13:56:26 +0530

    we know predicate is a functional interface so we can use lambda expression

--- src/featuresLearning/predicateInterface.java
+++ src/featuresLearning/predicateInterface.java
@@ -2,19 +2,26 @@ package featuresLearning;
 
 import java.util.function.Predicate;
 
-public class predicateInterface implements Predicate<Integer> {
+public class predicateInterface  {
 
-    @Override
-    public boolean test(Integer integer) {
-        if(integer>0)
-        {
-            return true;
-        }
-        return false;
-    }
+
 
     public static void main(String[] args) {
-        predicateInterface f= new predicateInterface();
-        System.out.println(f.test(5));
+       // predicateInterface f= new predicateInterface();
+        /*Predicate<Integer> p=(i)->{
+          if(i>10)
+          {
+              return true;
+          }
+          else{
+              return false;
+          }
+        };*/
+
+        // we can also do
+        Predicate<Integer> p=(i)->i>10;
+
+        // prediacte<T> is a functional interface with test() abstract method 1.e we can use lambda expression
+        System.out.println("Result: "+p.test(5));
     }
 }
