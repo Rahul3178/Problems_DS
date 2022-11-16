58ba54ff63da0ebb465416a8cbd95bd2e0cca9f5
Rahul <rk2670382@gmail.com>
2022-11-16 15:19:50 +0530

    for consumer interface return type alwasy void its only consumes value input value

---
+++ src/featuresLearning/ConsumerInterface.java
@@ -0,0 +1,11 @@
+package featuresLearning;
+
+import java.util.function.Consumer;
+
+public class ConsumerInterface
+{
+    public static void main(String[] args) {
+        Consumer<String> c=s->System.out.println(s);
+        c.accept("Rahul");
+    }
+}
