17f2dfe59674e7c66b59d11b8da0992ba31d4671
Rahul <rk2670382@gmail.com>
2022-11-16 15:39:02 +0530

    method reference with static method of main class

--- src/featuresLearning/methodReferenceEx.java
+++ src/featuresLearning/methodReferenceEx.java
@@ -1,14 +1,17 @@
 package featuresLearning;
 
+// with method reference
 public class methodReferenceEx
 {
-    public static void main(String[] args) {
-
-    Runnable r=()->{
-        for (int i = 0; i <=10; i++) {
-            System.out.println("Fromlambda Expression: "+i);
+    public static void m1()
+    {
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println("Child thread: "+i);
         }
-    };
+    }
+    public static void main(String[] args) {
+    Runnable r= methodReferenceEx::m1;
+
     Thread t= new Thread(r);
     t.start();
         for (int i = 0; i < 10; i++) {
@@ -16,3 +19,8 @@ public class methodReferenceEx
         }
     }
 }
+/*
+* in above example runnable interface run() method referring to main class\
+* static method m1() .
+* Method reference to instance method.
+* */
